7e12220510a75c147b93640fe83596b8abdb1a23
YuSec2021 <3179478769@qq.com>
2022-02-25 17:08:07 +0800

    blog Update

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: deploy.sh
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: Fri 25 Feb 2022 05:05:15 PM CST
+ ************************************************************************/
+
+
+ git add .
+ git commit -m "blog Update"
+ git push -u origin master
